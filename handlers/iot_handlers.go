6a10461a43298882df2ae9afee38e9b5a5d6d1ac
이민규 <67575226+suslmk-lee@users.noreply.github.com>
2024-12-04 17:25:53 +0900

    update Access-Control-Allow-Credentials

--- handlers/iot_handlers.go
+++ handlers/iot_handlers.go
@@ -159,6 +159,7 @@ func (h *IoTHandlers) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Accept")
+	w.Header().Set("Access-Control-Allow-Credentials", "true")
 
 	if r.Method == "OPTIONS" {
 		w.WriteHeader(http.StatusOK)
