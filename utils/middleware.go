fc86a40e450037c03de6cc785bbd71126e8de88c
이민규 <67575226+suslmk-lee@users.noreply.github.com>
2024-12-05 16:15:38 +0900

    update

--- utils/middleware.go
+++ utils/middleware.go
@@ -5,7 +5,7 @@ import "net/http"
 func CORSMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// CORS 헤더 설정
-		w.Header().Set("Access-Control-Allow-Origin", "http://133.186.135.247")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "*")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
